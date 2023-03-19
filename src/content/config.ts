c7e07e901f08211c56c6adcb1dd80c3f42c8f59e
Antonio <zolotuhin87@gmail.com>
2023-03-20 00:12:19 +0200

    me page: basic skeleton

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,16 @@
-import { defineCollection } from 'astro:content';
+import { defineCollection, z } from 'astro:content';
 
 const resume = defineCollection({});
+const me = defineCollection({
+	schema: z.object({
+		list: z.array(z.object({
+			name: z.string(),
+			icon: z.string()
+		})),
+	})
+});
 
 export const collections = {
-	resume: resume,
+	'resume': resume,
+	'me': me
 };
