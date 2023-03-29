33770d05b6022a12565f28a2ace2d5e1d6cf886e
Antonio <zolotuhin87@gmail.com>
2023-03-30 00:10:25 +0300

    refactor link component

---
+++ src/linksStorage.ts
@@ -0,0 +1,23 @@
+const username = 'bandantonio';
+export const github = {
+    name: 'GitHub',
+    url: `https://github.com/${username}`,
+    icon: 'ri:github-fill'
+}
+export const linkedin = {
+    name: 'LinkedIn',
+    url: `https://www.linkedin.com/in/${username}`,
+    icon: 'ri:linkedin-box-fill'
+}
+export const medium = {
+    name: 'Medium',
+    url: `https://${username}.medium.com`,
+    icon: 'ph:medium-logo-fill'
+}
+export const devto = {
+    name: 'Dev.to',
+    url: `https://dev.to/${username}`,
+    icon: 'mdi:dev-to'
+}
+
+export const platforms = [github, linkedin, medium, devto];
--- src/types.ts
+++ src/types.ts
@@ -2,6 +2,8 @@ export interface BlogPostFrontmatter {
     title: string
     date: string,
     img?: string,
-    excerpt: string
-    featured?: boolean
+    excerpt: string,
+    featured?: boolean,
+    medium?: string,
+    devto?: string,
 }
